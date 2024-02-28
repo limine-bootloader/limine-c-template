f55c482da440f55e67a3c1196968b259219e50c5
mintsuki <mintsuki@protonmail.com>
2024-02-28 15:41:38 +0100

    Revert back to using static volatile for requests

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -7,13 +7,14 @@
 // base revision described by the Limine boot protocol specification.
 // See specification for further info.
 
-LIMINE_BASE_REVISION(1)
+static volatile LIMINE_BASE_REVISION(1);
 
 // The Limine requests can be placed anywhere, but it is important that
-// the compiler does not optimise them away, so, in C, they should
-// NOT be made "static".
+// the compiler does not optimise them away, so, usually, they should
+// be made volatile or equivalent, _and_ they should be accessed at least
+// once.
 
-struct limine_framebuffer_request framebuffer_request = {
+static volatile struct limine_framebuffer_request framebuffer_request = {
     .id = LIMINE_FRAMEBUFFER_REQUEST,
     .revision = 0
 };
