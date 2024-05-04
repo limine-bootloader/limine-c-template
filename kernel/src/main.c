46514720e0505cb5525e7cb72109cda5f5139173
mintsuki <mintsuki@protonmail.com>
2024-05-04 02:07:17 +0200

    Updates for Limine 7.5.0 and requests delimiters

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -3,21 +3,32 @@
 #include <stdbool.h>
 #include <limine.h>
 
-// Set the base revision to 1, this is recommended as this is the latest
+// Set the base revision to 2, this is recommended as this is the latest
 // base revision described by the Limine boot protocol specification.
 // See specification for further info.
 
-static volatile LIMINE_BASE_REVISION(1);
+__attribute__((used, section(".requests")))
+static volatile LIMINE_BASE_REVISION(2);
 
 // The Limine requests can be placed anywhere, but it is important that
 // the compiler does not optimise them away, so, usually, they should
 // be made volatile or equivalent, _and_ they should be accessed at least
-// once.
+// once or marked as used with the "used" attribute as done here.
 
+__attribute__((used, section(".requests")))
 static volatile struct limine_framebuffer_request framebuffer_request = {
     .id = LIMINE_FRAMEBUFFER_REQUEST,
     .revision = 0
 };
+
+// Finally, define the start and end markers for the Limine requests.
+// These can also be moved anywhere, to any .c file, as seen fit.
+
+__attribute__((used, section(".requests_start_marker")))
+static volatile LIMINE_REQUESTS_START_MARKER;
+
+__attribute__((used, section(".requests_end_marker")))
+static volatile LIMINE_REQUESTS_END_MARKER;
 
 // GCC and Clang reserve the right to generate calls to the following
 // 4 functions even if they are not directly called.
