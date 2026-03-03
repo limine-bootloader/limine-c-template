3304a38f830657f800831c4019a9b8c8153036aa
Mintsuki <mintsuki@protonmail.com>
2026-03-04 00:47:42 +0100

    Add missing header guards to memory.h

--- kernel/src/memory.h
+++ kernel/src/memory.h
@@ -1,3 +1,6 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
 #include <stdint.h>
 #include <stddef.h>
 
@@ -5,3 +8,5 @@ void *memcpy(void *restrict dest, const void *restrict src, size_t n);
 void *memset(void *s, int c, size_t n);
 void *memmove(void *dest, const void *src, size_t n);
 int memcmp(const void *s1, const void *s2, size_t n);
+
+#endif
