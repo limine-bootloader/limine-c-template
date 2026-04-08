b67ae27827be05f647e36b29bb7a12c4b09c110d
Mintsuki <mintsuki@protonmail.com>
2026-04-08 05:39:05 +0200

    Remove useless void pointer casts from memory functions

--- kernel/src/memory.c
+++ kernel/src/memory.c
@@ -8,8 +8,8 @@
 // DO NOT remove or rename these functions, or stuff will eventually break!
 
 void *memcpy(void *restrict dest, const void *restrict src, size_t n) {
-    uint8_t *restrict pdest = (uint8_t *restrict)dest;
-    const uint8_t *restrict psrc = (const uint8_t *restrict)src;
+    uint8_t *restrict pdest = dest;
+    const uint8_t *restrict psrc = src;
 
     for (size_t i = 0; i < n; i++) {
         pdest[i] = psrc[i];
@@ -19,7 +19,7 @@ void *memcpy(void *restrict dest, const void *restrict src, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-    uint8_t *p = (uint8_t *)s;
+    uint8_t *p = s;
 
     for (size_t i = 0; i < n; i++) {
         p[i] = (uint8_t)c;
@@ -29,8 +29,8 @@ void *memset(void *s, int c, size_t n) {
 }
 
 void *memmove(void *dest, const void *src, size_t n) {
-    uint8_t *pdest = (uint8_t *)dest;
-    const uint8_t *psrc = (const uint8_t *)src;
+    uint8_t *pdest = dest;
+    const uint8_t *psrc = src;
 
     if ((uintptr_t)src > (uintptr_t)dest) {
         for (size_t i = 0; i < n; i++) {
@@ -46,8 +46,8 @@ void *memmove(void *dest, const void *src, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-    const uint8_t *p1 = (const uint8_t *)s1;
-    const uint8_t *p2 = (const uint8_t *)s2;
+    const uint8_t *p1 = s1;
+    const uint8_t *p2 = s2;
 
     for (size_t i = 0; i < n; i++) {
         if (p1[i] != p2[i]) {
